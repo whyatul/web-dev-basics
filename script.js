307d7d6e914e3efebe51a5952fbff37a8a89b695
Atul Maurya <atulmaurya18.04@gmail.com>
2025-06-04 09:34:18 +0530

    what js can do

---
+++ script.js
@@ -0,0 +1,5 @@
+function myfunc(){
+    document.getElementById('text3').innerHTML="This Text is written in External JavaScript File";
+    document.getElementById('text3').style.fontSize="40px";
+    document.getElementById('text3').style.color="yellow";
+}
